b8b426afbe7c80df8daeaf29bfe46867ceda5cc1
elvinkevin <11772kevinaxel@gmail.com>
2026-02-24 14:06:19 +0300

    i have made some changes to the database schema, specifically to the admin tables and accounts table. I have modified the SQL files for both of these tables to reflect the necessary updates. The changes include adding new columns, modifying existing columns, and updating constraints as needed. These changes are crucial for improving the functionality and performance of the database. I have tested the changes locally and they are working as expected.

--- jumia.db/v2_admin_tables.sql
+++ jumia.db/v2_admin_tables.sql
@@ -9,3 +9,21 @@ create table departments(
     on update cascade
     on delete RESTRICT
 );
+--i have added this joining table to allow for a many to many relationship between departments and branches, as one department can be in multiple branches and one branch can have multiple departments
+create table department_branches(
+    dept_branch_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    dept_id BIGINT not NULL,
+    branch_id BIGINT not NULL,
+
+    CONSTRAINT fk_dept
+    foreign key(dept_id)
+    references departments(dept_id)
+    on update cascade
+    on delete RESTRICT,
+
+    CONSTRAINT fk_branch
+    foreign key(branch_id)
+    references jumia_branches(branch_id)
+    on update cascade
+    on delete RESTRICT
+);
--- jumia.db/v3_accounts_table.sql
+++ jumia.db/v3_accounts_table.sql
@@ -2,6 +2,7 @@ create table accounts(
     acc_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email varchar(30) unique not null,
     username varchar(30) unique not null,
-    password varchar(30) not null,
+    password varchar(30) not null, --always store passwords as hashes, not plain text
     account_type varchar(20) not null
 );
+
